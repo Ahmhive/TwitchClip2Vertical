d08e18534cb863250b91b32c521e5770a510af54
Ahmhive <117471277+Ahmhive@users.noreply.github.com>
2023-09-15 02:19:44 +0100

    Update getvods.py

--- videos/getvods.py
+++ videos/getvods.py
@@ -4,7 +4,7 @@ import os
 import csv
 from random import randint
 from datetime import datetime, timedelta
-
+import sys
 
 def find_peak_times(vod):
 
@@ -50,48 +50,49 @@ def find_peak_times(vod):
 
 
 
-def main(vodlinks):
-
-    vod_links = ('https://www.twitch.tv/videos/1896762513','https://www.twitch.tv/videos/1894994632','https://www.twitch.tv/videos/1894155462','https://www.twitch.tv/videos/1893713165','https://www.twitch.tv/videos/1891564537')
-
-    file_1= 'cliptime.csv'
-
-    with open(file_1, 'w+') as filehandle:
-        filehandle.write("")
-
-    for vod in vod_links:
-        peak_times = find_peak_times(vod)
-        for time in peak_times:
-            time_start = time + timedelta(seconds=45)
-            time_end = time - timedelta(seconds=45) 
-            try:
-                with open(file_1, 'a+') as filehandle: ##a+ to keep old w+ to start new
-                    filehandle.write('%s,%s,%s,%s\n' % (vod, time, time_start, time_end))
-            except:
-                print('errorfile')
-            #print(vod, time)
-
-    #os.system("cd videos")
-
-    with open('cliptime.csv') as csvfile:
-        csvstring = csvfile.read() + '\n'
-        lines = csvstring.splitlines()
-        reader = csv.reader(lines)
-        csvdata = list(reader)
 
 
-    for clip_no in range(0,59):
-        link = csvdata[clip_no][0]
-        #Mid_time = csvdata[clip_no][1]
-        start_time = csvdata[clip_no][3]
-        end_time = csvdata[clip_no][2]
+vod_links = (sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
 
-        s_t = start_time[11:19]
-        e_t = end_time[11:19]
-        #twitch-dl download <videos> -q 720p -s hh:mm:ss -e hh:mm:ss
-        command1 = f"twitch-dl download {link} -f mp4 -q source -s {s_t} -e {e_t}"+" -o {channel}-"+f"{clip_no}"+".{format}"
-        command = command1
-        print(command)
-    
-        os.system(command)
+file_1= 'cliptime.csv'
+
+with open(file_1, 'w+') as filehandle:
+    filehandle.write("")
+
+for vod in vod_links:
+    peak_times = find_peak_times(vod)
+    for time in peak_times:
+        time_start = time + timedelta(seconds=45)
+        time_end = time - timedelta(seconds=45) 
+        try:
+            with open(file_1, 'a+') as filehandle: ##a+ to keep old w+ to start new
+                filehandle.write('%s,%s,%s,%s\n' % (vod, time, time_start, time_end))
+        except:
+            print('errorfile')
+        #print(vod, time)
+
+#os.system("cd videos")
+
+with open('cliptime.csv') as csvfile:
+    csvstring = csvfile.read() + '\n'
+    lines = csvstring.splitlines()
+    reader = csv.reader(lines)
+    csvdata = list(reader)
+
+
+for clip_no in range(0,59):
+    link = csvdata[clip_no][0]
+    #Mid_time = csvdata[clip_no][1]
+    start_time = csvdata[clip_no][3]
+    end_time = csvdata[clip_no][2]
+
+    s_t = start_time[11:19]
+    e_t = end_time[11:19]
+    #twitch-dl download <videos> -q 720p -s hh:mm:ss -e hh:mm:ss
+    command1 = f"twitch-dl download {link} -f mp4 -q source -s {s_t} -e {e_t}"+" -o {channel}-"+f"{clip_no}"+".{format}"
+    command = command1
+    print(command)
+
+    os.system(command)
+ 
  
